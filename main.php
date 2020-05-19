ad27d868f5b33a21ff5ec8fd6d9fa393897dd310
Alexey Borohvostov <troizet@gmail.com>
2020-05-19 14:24:14 +0700

    add proxy scraper

--- main.php
+++ main.php
@@ -1,59 +1,69 @@
 <?php
 
-$arr = [
-      'a',
-      'b',
-      'c',
-      'd',
-      'f',
-      'g',
-];
-$z = 0;
+use GuzzleHttp\Client as GuzzleClient;
+use Http\Adapter\Guzzle6\Client as HttpAdapter;
+use Http\Message\MessageFactory\GuzzleMessageFactory as MessageFactory;
+use Vantoozz\ProxyScraper\HttpClient\Psr18HttpClient;
+use Vantoozz\ProxyScraper\Scrapers;
 
-$proxy_list = [
-    [
-        'socks5_host' => '47.90.74.82',
-        'socks5_port' => '29588'
-    ],
-    [
-        'socks5_host' => '72.11.148.222',
-        'socks5_port' => '56533'
-    ],
-    [
-        'socks5_host' => '5.56.61.183',
-        'socks5_port' => '51295'
-    ],
-    [
-        'socks5_host' => '216.144.228.130',
-        'socks5_port' => '15378'
-    ],
-    [
-        'socks5_host' => '104.236.26.27',
-        'socks5_port' => '38801'
-    ]
-];
+require_once __DIR__ . '/vendor/autoload.php';
+
+$httpClient = new Psr18HttpClient(
+    new HttpAdapter(new GuzzleClient([
+        'connect_timeout' => 2,
+        'timeout' => 3,
+    ])),
+    new MessageFactory
+);
 $run_main = new Swoole\Coroutine\Scheduler;
+$compositeScraper = new Scrapers\CompositeScraper;
 
-//while($z <= 2) {
+$compositeScraper->addScraper(new Scrapers\SocksProxyScraper($httpClient));
 
-    for ($j = 0; $j < 5; $j++) {
-        $proxy = $proxy_list[$j];
-        $run_main->add(function () use ($proxy) {
-            echo 'Run worker' . PHP_EOL;
-            for ($i = 0; $i < 11; $i++) {
-                go(function () use ($proxy) {
-                    $client = new Swoole\Coroutine\Http\Client('crator.z4c.ru', 80);
-                    $client->set($proxy);
-                    $client->set(['timeout' => 10]);
-                    $client->get('/e/call_me?phone=45345345');
-                    echo 'Proxy ' . $proxy['socks5_host'] . ' response ' . $client->body . PHP_EOL;
-                    $client->close();
-                });
-            }
-            echo 'End worker' . PHP_EOL;
-        });
+$proxy_list = [];
+while (true) {
+    echo 'Load proxy list' . PHP_EOL;
+    foreach ($compositeScraper->get() as $proxy) {
+        // echo $proxy . "\n";
+        $proxy = explode(':', $proxy);
+        $proxy_list[] = [
+            'socks5_host' => $proxy[0],
+            'socks5_port' => $proxy[1]
+        ];
     }
-    $run_main->start();
-    $z++;
-//}
+    echo 'Proxy list loaded'.PHP_EOL;
+    sleep(5);
+
+
+
+    $proxy_count = count($proxy_list) - 1;
+    echo 'Proxy count ' . $proxy_count . PHP_EOL;
+    sleep(5);
+    echo 'Work started '.PHP_EOL;
+    sleep(5);
+    while ($proxy_count > 0) {
+
+        for (; $proxy_count > 0; $proxy_count--) {
+            $proxy = $proxy_list[$proxy_count];
+            $run_main->add(function () use ($proxy) {
+                // echo 'Run worker' . PHP_EOL;
+                for ($i = 0; $i < 11; $i++) {
+                    go(function () use ($proxy) {
+                        $client = new Swoole\Coroutine\Http\Client('crator.z4c.ru', 80);
+                        $client->set($proxy);
+                        $client->set(['timeout' => 10]);
+                        $client->get('/e/call_me?phone=45345345');
+                        echo 'Proxy ' . $proxy['socks5_host'] . ' response ' . $client->body . PHP_EOL;
+                        $client->close();
+                    });
+                }
+                // echo 'End worker' . PHP_EOL;
+            });
+        }
+        $run_main->start();
+    }
+    echo 'Work ended '.PHP_EOL;
+    sleep(5);
+    unset($proxy_list);
+}
 
