0117d536faf20a254f18daf132c29196e60f11fd
Alexey Borohvostov <troizet@gmail.com>
2020-05-19 16:27:23 +0700

    show status code

--- main.php
+++ main.php
@@ -1,5 +1,5 @@
 <?php
-
+//http://spys.one/en/socks-proxy-list/
 use GuzzleHttp\Client as GuzzleClient;
 use Http\Adapter\Guzzle6\Client as HttpAdapter;
 use Http\Message\MessageFactory\GuzzleMessageFactory as MessageFactory;
@@ -19,19 +19,24 @@ $run_main = new Swoole\Coroutine\Scheduler;
 $compositeScraper = new Scrapers\CompositeScraper;
 
 $compositeScraper->addScraper(new Scrapers\SocksProxyScraper($httpClient));
+//$compositeScraper->addScraper(new Scrapers\CoolProxyScraper($httpClient));
+//$compositeScraper->addScraper(new Scrapers\FreeProxyListScraper($httpClient));
 
 $proxy_list = [];
 while (true) {
     echo 'Load proxy list' . PHP_EOL;
     foreach ($compositeScraper->get() as $proxy) {
         // echo $proxy . "\n";
+      //  print_r($proxy->getMetrics());
         $proxy = explode(':', $proxy);
+
         $proxy_list[] = [
             'socks5_host' => $proxy[0],
             'socks5_port' => $proxy[1]
         ];
     }
     echo 'Proxy list loaded'.PHP_EOL;
+   // print_r($proxy_list);
     sleep(5);
 
 
@@ -51,16 +56,25 @@ while (true) {
                     go(function () use ($proxy) {
                         $client = new Swoole\Coroutine\Http\Client('crator.z4c.ru', 80);
                         $client->set($proxy);
-                        $client->set(['timeout' => 10]);
+                        $client->set(['timeout' => 20]);
                         $client->get('/e/call_me?phone=45345345');
-                        echo 'Proxy ' . $proxy['socks5_host'] . ' response ' . $client->body . PHP_EOL;
+                       // echo $client->statusCode.PHP_EOL;
+                       /*
+                            HTTP_CLIENT_ESTATUS_CONNECT_FAILED = -1,
+                            HTTP_CLIENT_ESTATUS_REQUEST_TIMEOUT = -2,
+                            HTTP_CLIENT_ESTATUS_SERVER_RESET = -3,
+                            HTTP_CLIENT_ESTATUS_SEND_FAILED = -4,
+                       */
+                        echo 'Proxy ' . $proxy['socks5_host'] . ' response ' . $client->body . ' with status ' .$client->statusCode.PHP_EOL;
                         $client->close();
                     });
                 }
                 // echo 'End worker' . PHP_EOL;
             });
+           // sleep(5);
         }
         $run_main->start();
+        exit(0);
     }
     echo 'Work ended '.PHP_EOL;
     sleep(5);
